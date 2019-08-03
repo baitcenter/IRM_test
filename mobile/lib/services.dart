9ee86eb825a8c783c11e43aba0dfb868f63b7af1
quangle <quangle.bxl@gmail.com>
2019-08-03 21:15:03 +0200

    clean up files

--- mobile/lib/services.dart
+++
@@ -1,3 +0,0 @@
-export 'package:irm_test/services/auth_service/auth_service.dart';
-export 'package:irm_test/services/auth_service/auth_service_firebase.dart';
-export 'package:irm_test/services/service_provider.dart';
