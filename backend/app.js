80920611793bd230f9f158691a2ee5cda4198700
quangle <quangle.bxl@gmail.com>
2019-08-05 14:03:08 +0200

    create user and get user OK

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app=express();
 const dotenv=require('dotenv').config();
 
 //please secure this
-let url = "mongodb://localhost:27017/dorothycares";
+let url = "mongodb://master:ceM7kFQCWkdLZcUEXkDaKhkg@ds016108.mlab.com:16108/test-irm";
 const port = process.env.PORT || 5000;
 
 var myDB;//global var for the DB, not so clean
@@ -13,10 +13,44 @@ let mongodb = require ('mongodb').MongoClient;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+mongodb.connect(url, { useNewUrlParser: true },function(err,db,){
+    if (err) throw err;
+    myDB=db.db("test-irm");
+    app.listen(port, function(){
+        console.log ('listening to '+ port)
+    });
+})
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/getuser',(req,res) => res.send('Get user details from mongoDB'));
-app.post('/createuser', (req,res)=> res.send('Create new user in MongoDB'));
+app.get('/getuser',(req,res) => {
+    let uid = req.query.uid;
+    let queryResult=[];
+    myDB.collection('team')
+    .find({"uid":uid})
+    .toArray(function(err,result){
+        if (err) {
+            res.status(418);
+            throw err
+        };
+        queryResult=result;
+        queryResult=JSON.stringify(queryResult);
+        console.log(queryResult);
+        res.status(200);
+        return res.send(queryResult);
+    })
+});
+app.post('/createuser', (req,res)=> {
+    //needs sanitization 
+    myDB.collection('team').insertOne(req.body,(err,result)=>{
+        if (err) {
+            res.status(418) 
+            return res.send(err)};
+        console.log('saved to database');
+        return res.status(200)
+
+    })
+});
 app.post('/updateuser',(req,res)=>res.send('update user details in MongoDB'));
 app.delete('/userdelete',(req,res)=>res.send('delete user in MongoDB'));
 
@@ -26,4 +60,3 @@ app.post('/updateevent',(req,res)=>res.send('update event details in MongoDB'));
 app.delete('/eventdelete',(req,res)=>res.send('delete event in MongoDB'));
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
