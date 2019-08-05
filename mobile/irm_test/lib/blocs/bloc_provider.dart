70c3d3569f3636eade5336f35be17fc336068623
quangle <quangle.bxl@gmail.com>
2019-08-05 17:34:55 +0200

    refactor BLoc

--- mobile/irm_test/lib/blocs/bloc_provider.dart
+++ mobile/irm_test/lib/blocs/bloc_provider.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:irm_test/blocs/agenda_bloc.dart';
 import 'package:irm_test/blocs/app_bloc.dart';
 import 'package:irm_test/blocs/calendar_bloc.dart';
 
 class BlocProvider extends InheritedWidget {
   final AppBloc appBloc;
-  final AgendaBloc agendaBloc;
   final CalendarBloc calendarBloc;
 
   const BlocProvider({
     Key key,
-    @required this.agendaBloc,
     @required this.appBloc,
     @required this.calendarBloc,
     @required Widget child,
   })  : assert(child != null),
         assert(appBloc != null),
-        assert(agendaBloc != null),
         assert(calendarBloc != null),
         super(key: key, child: child);
 
