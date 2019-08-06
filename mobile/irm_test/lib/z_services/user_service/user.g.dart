ca8f4960437f4c53c5d7113a3a1fb8d86d90f5ca
quangle <quangle.bxl@gmail.com>
2019-08-06 14:35:53 +0200

    get names on dropdown button

--- mobile/irm_test/lib/z_services/user_service/user.g.dart
+++ mobile/irm_test/lib/z_services/user_service/user.g.dart
@@ -6,12 +6,14 @@ part of 'user.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-User _$UserFromJson(Map<String, dynamic> json) {
+User _$UserFromJson(Map json) {
   return User(
     userName: json['userName'] as String,
     calendar: json['calendar'] == null
         ? null
-        : Calendar.fromJson(json['calendar'] as Map<String, dynamic>),
+        : Calendar.fromJson((json['calendar'] as Map)?.map(
+            (k, e) => MapEntry(k as String, e),
+          )),
     uid: json['uid'] as String,
   );
 }
